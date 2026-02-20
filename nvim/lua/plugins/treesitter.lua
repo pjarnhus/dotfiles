26758bef69fe5f9e070dea0a3e2cc51528c7cd0b
Philip R. Jarnhus <jarnhus@gmail.com>
2026-02-20 21:09:34 +0100

    Fix error in help window
    Whenever the help was opened, an error occurred. This was because the
    treesitter plugin did not ensure that vimdoc parser was installed.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {{
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-            ensure_installed = { "python", "markdown", "lua"},
+            ensure_installed = { "python", "markdown", "lua", "vimdoc"},
             sync_install = false,
             highlight = {
                 enable = true,
